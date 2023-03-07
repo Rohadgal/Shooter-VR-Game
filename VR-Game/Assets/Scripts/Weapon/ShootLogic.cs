c304ab35dbe3c7c6a0e775d4d4fc60d325c059b3
04WS-LAB202-04\Alumno <jan.acevedo@artek.edu.mx>
2023-03-07 13:00:39 -0600

    Disparo actualizado

--- VR-Game/Assets/Scripts/Weapon/ShootLogic.cs
+++ VR-Game/Assets/Scripts/Weapon/ShootLogic.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootLogic : MonoBehaviour
 {
+    public Image slider;
     public Transform destino;
     public Transform origen;
     public int tiempo;
@@ -13,7 +15,11 @@ public class ShootLogic : MonoBehaviour
     private Vector3 puntoParabola;
     private Vector3 velocidadInicial;
     private bool estoyDisparando = false;
+    public bool shootEnabled = true;
+    public bool test;
     public float timer = 1;
+    public float cooldownTimer;
+    public float maxCooldown = 7;
 
 
 
@@ -22,6 +28,8 @@ public class ShootLogic : MonoBehaviour
     void Start()
     {
         lineRenderer= GetComponent<LineRenderer>();
+        shootEnabled = true;
+
     }
 
     void Update()
@@ -34,23 +42,84 @@ public class ShootLogic : MonoBehaviour
 
         }
 
-        Shoot();
+        if(shootEnabled == true)
+        {
+            Shoot();
+        }
+
+
+
+        if (test == true)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+
+        if(shootEnabled == false)
+        {
+            test = true;
+        }
+
+        if(cooldownTimer <= 0)
+        {
+            test = false;
+            shootEnabled = true;
+        }
+
+        RefreshSlider();
     }
 
     public void Shoot()
     {
-        if (Input.GetMouseButton(0) && timer == 1)
+        if (Input.GetMouseButton(0) && timer >= 0.5f)
         {
             GameObject bola = Instantiate(bolaPrefab, origen.transform.position, Quaternion.identity);
             bola.GetComponent<Rigidbody>().velocity = velocidadInicial;
             timer = 0;
 
+            
         }
 
         if (Input.GetMouseButton(0))
         {
             timer += Time.deltaTime;
+            cooldownTimer += Time.deltaTime;
+            test = false;
         }
+        else
+        {
+            test = true;
+        }
+        
+
+        if (cooldownTimer >= 7)
+        {
+            shootEnabled = false;
+        }
+
+        if(cooldownTimer <= 0)
+        {
+            test = false;
+        }
+    }
+
+    public void cooldownRefresh()
+    {
+        
+         cooldownTimer -= Time.deltaTime;
+        
+    }
+    public void ShootCooldown()
+    {
+        if(Input.GetMouseButton(0) && cooldownTimer >= 0)
+        {
+            cooldownTimer += Time.deltaTime;
+        }
+
+    }
+
+    public void RefreshSlider()
+    {
+        slider.fillAmount = cooldownTimer / maxCooldown;
     }
     private Vector3 VelocidadInicialCalculo(Vector3 destino,Vector3 origen, float tiempo)
     {
