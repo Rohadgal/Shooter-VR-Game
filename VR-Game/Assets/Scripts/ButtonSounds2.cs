6201e40ce156a768cc6d0d16111ababbe548c46e
Gal <gal.frias@gmail.com>
2023-03-22 13:10:55 -0600

    Fonts

---
+++ VR-Game/Assets/Scripts/ButtonSounds2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSounds2 : MonoBehaviour
+{
+    public AudioSource audioSource1;
+    public AudioClip clip1;
+    public AudioSource audioSource2;
+    public AudioClip clip2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource1.clip = clip1;
+        audioSource2.clip = clip2;
+
+    }
+
+    public void PlaySound()
+    {
+        audioSource1.Play();
+    }
+
+    public void BackgroundMusic()
+    {
+        audioSource2.Play();
+    }
+
+    /*public void StopMusic()
+    {
+        audioSource2.Pause();
+    }*/
+}
+
